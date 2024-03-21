433a94cfded76ebd706e590e25295c6b87345d9a
Abhishek Nagesh <abhishek.nagesh@tarento.com>
2024-03-21 13:01:07 +0530

    Issue #ED-3836: changed export structure and config changes

--- src/sui.module.ts
+++ src/sui.module.ts
@@ -1,39 +1,31 @@
 import { NgModule } from "@angular/core";
 
 // Collections
-import {
-    SuiMessageModule,
-    SuiPaginationModule
-} from "./collections/internal";
+import { SuiPaginationModule } from "./collections/pagination/pagination.module";
+import { SuiMessageModule } from "./collections/message/message.module";
 
 // Modules
-import {
-    SuiAccordionModule,
-    SuiCheckboxModule,
-    SuiCollapseModule,
-    SuiDatepickerModule,
-    SuiDimmerModule,
-    SuiDropdownModule,
-    SuiModalModule,
-    SuiPopupModule,
-    SuiProgressModule,
-    SuiRatingModule,
-    SuiSearchModule,
-    SuiSidebarModule,
-    SuiTabsModule,
-    SuiSelectModule,
-    SuiTransitionModule
-} from "./modules/internal";
+import { SuiAccordionModule } from "./modules/accordion/accordion.module";
+import { SuiCheckboxModule } from "./modules/checkbox/checkbox.module";
+import { SuiCollapseModule } from "./modules/collapse/collapse.module";
+import { SuiDatepickerModule } from "./modules/datepicker/datepicker.module";
+import { SuiDimmerModule } from "./modules/dimmer/dimmer.module";
+import { SuiDropdownModule } from "./modules/dropdown/dropdown.module";
+import { SuiModalModule } from "./modules/modal/modal.module";
+import { SuiPopupModule } from "./modules/popup/popup.module";
+import { SuiProgressModule } from "./modules/progress/progress.module";
+import { SuiRatingModule } from "./modules/rating/rating.module";
+import { SuiSearchModule } from "./modules/search/search.module";
+import { SuiSidebarModule } from "./modules/sidebar/sidebar.module";
+import { SuiTabsModule } from "./modules/tabs/tab.module";
+import { SuiSelectModule } from "./modules/select/select.module";
+import { SuiTransitionModule } from "./modules/transition/transition.module";
 
 // Behaviors
-import {
-    SuiLocalizationModule
-} from "./behaviors/internal";
+import { SuiLocalizationModule } from "./behaviors/localization/localization.module";
 
 // Misc
-import {
-    SuiUtilityModule
-} from "./misc/internal";
+import { SuiUtilityModule } from "./misc/util/util.module";
 
 @NgModule({
     exports: [
@@ -65,4 +57,4 @@ import {
         SuiUtilityModule
     ]
 })
-export class SuiModule {}
+export class SuiModule { }

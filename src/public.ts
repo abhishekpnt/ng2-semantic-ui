c040ca955db6f6e22f164b60f998b85985d7a62f
Abhishek Nagesh <abhishek.nagesh@tarento.com>
2024-03-21 14:35:16 +0530

    Issue #ED-3836: Angular-10 upgrade with ivy

--- src/public.ts
+++ src/public.ts
@@ -1,6 +1,128 @@
-export * from "./behaviors/public";
-export * from "./collections/public";
-export * from "./modules/public";
 
-export * from "./misc/public";
 export { SuiModule } from "./index";
+
+export * from "./behaviors/localization/locales/interfaces/values";
+export * from "./behaviors/localization//services/localization.service";
+export * from "./behaviors/localization//localization.module";
+
+export * from "./collections/message/components/message";
+export * from "./collections/message/message.module";
+
+
+export * from "./collections/pagination/components/pagination";
+export * from "./collections/pagination/pagination.module";
+
+export * from "./misc/util/helpers/custom-validator";
+export * from "./misc/util/helpers/custom-value-accessor";
+export * from "./misc/util/helpers/date";
+export * from "./misc/util/helpers/focus-event";
+
+export * from "./misc/util/helpers/util";
+export * from "./misc/util/services/component-factory.service";
+export * from "./misc/util/services/positioning.service";
+export * from "./misc/util/util.module";
+
+export * from "./modules/accordion/components/accordion-panel";
+export * from "./modules/accordion/components/accordion";
+export * from "./modules/accordion/services/accordion.service";
+export * from "./modules/accordion/accordion.module";
+
+export * from "./modules/checkbox/components/checkbox";
+export * from "./modules/checkbox/components/radio";
+export * from "./modules/checkbox/directives/radio-manager";
+export * from "./modules/checkbox/checkbox.module";
+
+export * from "./modules/collapse/directives/collapse";
+export * from "./modules/collapse/collapse.module";
+
+export * from "./modules/datepicker/classes/calendar-config";
+export * from "./modules/datepicker/classes/calendar-mappings";
+export * from "./modules/datepicker/classes/date-comparer";
+export * from "./modules/datepicker/classes/date-parser";
+// export * from "./modules/datepicker/classes/datepicker-mode";
+
+
+export * from "./modules/datepicker/components/calendar-view-title";
+export * from "./modules/datepicker/components/datepicker";
+
+export * from "./modules/datepicker/directives/calendar-item";
+export * from "./modules/datepicker/directives/datepicker.directive";
+export * from "./modules/datepicker/directives/input.directive";
+
+export * from "./modules/datepicker/helpers/date-fns";
+
+export * from "./modules/datepicker/services/calendar-range.service";
+export * from "./modules/datepicker/services/calendar.service";
+
+export * from "./modules/datepicker/views/calendar-view";
+export * from "./modules/datepicker/views/date-view";
+export * from "./modules/datepicker/views/hour-view";
+export * from "./modules/datepicker/views/minute-view";
+export * from "./modules/datepicker/views/month-view";
+export * from "./modules/datepicker/views/year-view";
+
+export * from "./modules/datepicker/datepicker.module";
+
+export * from "./modules/dimmer/components/dimmer";
+export * from "./modules/dimmer/dimmer.module";
+
+export * from "./modules/dropdown/directives/dropdown-menu";
+export * from "./modules/dropdown/directives/dropdown";
+export * from "./modules/dropdown/services/dropdown.service";
+export * from "./modules/dropdown/dropdown.module";
+
+export * from "./modules/modal/classes/active-modal";
+export * from "./modules/modal/classes/modal-config";
+export * from "./modules/modal/classes/modal-controls";
+export * from "./modules/modal/classes/modal-template";
+export * from "./modules/modal/components/modal";
+export * from "./modules/modal/services/modal.service";
+export * from "./modules/modal/modal.module";
+
+export * from "./modules/popup/classes/popup-component-controller";
+export * from "./modules/popup/classes/popup-config";
+export * from "./modules/popup/classes/popup-controller";
+export * from "./modules/popup/classes/popup-lifecycle";
+export * from "./modules/popup/classes/popup-template-controller";
+export * from "./modules/popup/components/popup-arrow";
+export * from "./modules/popup/components/popup";
+export * from "./modules/popup/directives/popup.directive";
+export * from "./modules/popup/services/popup.service";
+export * from "./modules/popup/popup.module";
+
+export * from "./modules/progress/components/progress";
+export * from "./modules/progress/progress.module";
+
+export * from "./modules/rating/components/rating";
+export * from "./modules/rating/rating.module";
+
+export * from "./modules/search/components/search-result";
+export * from "./modules/search/components/search";
+export * from "./modules/search/helpers/lookup-fn";
+export * from "./modules/search/services/search.service";
+export * from "./modules/search/search.module";
+
+export * from "./modules/select/classes/select-base";
+export * from "./modules/select/components/multi-select-label";
+export * from "./modules/select/components/multi-select";
+export * from "./modules/select/components/select-option";
+export * from "./modules/select/components/select";
+export * from "./modules/select/directives/select-search";
+export * from "./modules/select/select.module";
+
+export * from "./modules/sidebar/components/sidebar-container";
+export * from "./modules/sidebar/components/sidebar-sibling";
+export * from "./modules/sidebar/components/sidebar";
+export * from "./modules/sidebar/services/sidebar.service";
+export * from "./modules/sidebar/sidebar.module";
+
+export * from "./modules/tabs/classes/tab";
+export * from "./modules/tabs/components/tabset";
+export * from "./modules/tabs/directives/tab-content";
+export * from "./modules/tabs/directives/tab-header";
+export * from "./modules/tabs/tab.module";
+
+export * from "./modules/transition/classes/transition-controller";
+export * from "./modules/transition/classes/transition";
+export * from "./modules/transition/directives/transition";
+export * from "./modules/transition/transition.module";
